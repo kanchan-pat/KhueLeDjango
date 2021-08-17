43811ade2ba705813bfa8d3057f3c55f6cb58833
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2021-08-17 14:33:45 -0400

    # Please add product image and handle displaying it in templates

--- Inventory App/inventory_app/productapp/models.py
+++ Inventory App/inventory_app/productapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 # Create your models here.
-
+# Please add product image and handle displaying it in templates 
 class product(models.Model):#ORM established
     name = models.CharField(max_length=55)
     description = models.CharField(max_length=2000)
